c12e449e3f4d633f3a3d8d067983c5490f0f6707
gianninart <118191110+gianninart@users.noreply.github.com>
2022-12-13 20:51:29 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,18 @@
-/* 
-var menuButton = document.querySelector("header > #menu");
-var menu = document.querySelector ("header nav ul");
-var openButtonImage = document.querySelector("header > #menu > img");
-
-function toggleMenu(){
-    menu.classList.toggle("menuOpen");
-}
-
-if(openButtonImage.src === "./images/menu.png"){
-    openButtonImage.src = "./images/close.png"
-} else{
-    openButtonImage = "./images/menu.png"
-}
-
-menuButton.addEventListener("click",toggleMenu) */
-
-var openButton = document.querySelector("header > #menu");
+var openButton = document.querySelector("header > button:nth-of-type(2)");
+var closeButton = document.querySelector("header nav button" );
+var deNav = document.querySelector("nav");
 
 // stap 2: laat de menu-button luisteren naar kliks en voer dan een functie uit
 openButton.addEventListener("click", openMenu);
+closeButton.addEventListener("click", closeMenu);
 
 // stap 3: voeg in de functie een class toe aan de nav
 function openMenu() {  
-  // zoek de nav op
-  var deNav = document.querySelector("nav");
   // voeg class toe aan nav
   deNav.classList.add("toonMenu");
+}
+
+function closeMenu(){
+  deNav.classList.remove("toonMenu");
 }
 
